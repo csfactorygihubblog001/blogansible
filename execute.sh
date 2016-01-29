8dfbf772cadf27734bd8ecfafe7faac5c6391e42
hideki-uemura <h-uemura@cs-fact.com>
2016-01-30 02:30:15 +0900

    openvpnのテスト実装完了 ・openvpnの設定を一旦削除して再設定するできるように変更(execute.shでextra-vars指定するようにした) ・yumでのインストールをstateをlatestからpresentへ変更 ・openvpnサービスにstopが間違っていた ・対話型自動化(expect)xxxxx.expファイルのパーミッションをrootだけに変更 ・コメントがまちがっていたものを変更 ・出来上がったサーバ/CA/DH/失効リストなどのパーミッションを500に変更
    server.confにVPNのネットワークIPを直に書いていたがenviroment.ymlにうつし
    環境依存とした。

--- execute.sh
+++ execute.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+declare -A extra_hash
+extra_hash["reinstall_openvpn"]="yes"
+extra_hash["reinstall_selenium"]="no"
+extra_vars=""
+for key in ${!extra_hash[@]}; do
+	tmp="\"${key}\" : \"${extra_hash[$key]}\""
 
+	if [ "x${extra_vars}" = "x" ];then
+		extra_vars=$tmp
+	else
+		extra_vars=$extra_vars,$tmp
+	fi
+done
 
-
-echo "Ansible実行します。"
-ansible-playbook ./site.yml
+	if [ "x$extra_vars" != "x" ];then
+		extra_vars="{ $extra_vars }"
+		echo "Ansible実行します。パラメータ$extra_vars"
+		ansible-playbook ./site.yml  --extra-vars "$extra_vars"
+	else
+		echo "Ansible実行します。パラメータ無し"
+		ansible-playbook ./site.yml
+	fi
